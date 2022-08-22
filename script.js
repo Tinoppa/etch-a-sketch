98c1dd442c8b17ee28288f479d6392eb9dc85d5c
Tinoppa <tino.kalliola@gmail.com>
2022-08-22 16:00:13 +0300

    Change layout and add reset function

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const container = document.getElementById('container');
+const btn = document.getElementById('set');
+const resetBtn = document.getElementById('reset');
+let userRows;
+let userCols = userRows;
+makeRows(16,16);
+
+function init() {
+    
+window.location.reload();
+
+}
+
+resetBtn.addEventListener('click', function (){
+    init();
+})
+
 
 function makeRows(rows,cols){
     container.style.setProperty('--grid-rows', rows);
@@ -6,28 +22,35 @@ function makeRows(rows,cols){
 
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement('div');
-        cell.innerText = (i + 1);
         container.appendChild(cell).className = 'grid-item';
+        
     }
+
+   
 }
 
+btn.addEventListener('click', function(){
+     userRows = parseInt(prompt('Set a number'));
+     userCols = userRows;
+    console.log(userCols);
+    makeRows(userRows,userCols);
+    if (userRows > 100) {
+        init();
+        alert("Way too much, try again");
+    }
+    
+});
 
-makeRows(16,16);
 
 const divs = document.getElementsByClassName('grid-item');
 
 for (const div of divs) {
-    div.addEventListener('click', function onClick() {
-      console.log('box clicked');
-      divs.style.color ="red";
+    div.addEventListener('mouseover', function onHover() {
+      this.style.backgroundColor = 'black';
+
+        
     });
+    
+    
   }
 
-/*for (let i = 0; i < div.length; i++) {
-    div[i].addEventListener('mouseover', function () {
-        div.style.backgroundColor = "red"
-    });
-    div[i].addEventListener('mouseleave', function () {
-        div.style.backgroundColor = 'white';
-    });
-}*/
